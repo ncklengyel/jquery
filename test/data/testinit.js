99fe3ac7ceee74c04bc5bae81c6df49470b97967
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2021-03-24 22:11:22 +0100

    WIP Event: Make selector module similar to selector-native from jQuery 3.x

--- test/data/testinit.js
+++ test/data/testinit.js
@@ -295,13 +295,13 @@ QUnit.basicTests = ( QUnit.urlParams.module + "" ) === "basic";
 // Says whether jQuery positional selector extensions are supported.
 // A full selector engine is required to support them as they need to be evaluated
 // left-to-right. Remove that property when support for positional selectors is dropped.
-QUnit.jQuerySelectorsPos = true;
+QUnit.jQuerySelectorsPos = false;
 
 // Says whether jQuery selector extensions are supported. Change that to `false`
 // if your custom jQuery versions relies more on native qSA.
 // This doesn't include support for positional selectors (see above).
 // TODO do we want to keep this or just assume support for jQuery extensions?
-QUnit.jQuerySelectors = true;
+QUnit.jQuerySelectors = false;
 
 // Support: IE 11+
 // A variable to make it easier to skip specific tests in IE, mostly
